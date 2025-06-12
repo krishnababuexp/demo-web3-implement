dfc64bbd62cb5fbba1e743fa29a168d47d8d4d2d
krishna-bhandari <krishnabhandarigit@outlook.com>
2025-06-12 12:53:10 +0545

    save

--- src/constants/constants.js
+++
@@ -1,9 +0,0 @@
-export const NFT_ADDRESS = "0xYourNFTAddress";
-export const MARKETPLACE_ADDRESS = "0xYourMarketplaceAddress";
-
-export const nftAbi = [
-  /* paste NFT ABI here */
-];
-export const marketplaceAbi = [
-  /* paste Marketplace ABI here */
-];
